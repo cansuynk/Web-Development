c23033226c7e2a00f14a38b7c330e762eeecafea
cansuynk <cansuyanik96@gmail.com>
2021-11-02 11:55:05 +0300

    Project updated

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,16 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request'); 
 const https = require('https');
-//const mailchimp = require("@mailchimp/mailchimp_marketing");
 
-const port = 3000;
+
+/**Works both in heroku's system and our port */
+const port = process.env.PORT || 3000;
 const app = express();
 
 /**to be able to use static files (css, images etc.) */
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
-//mailchimp.setConfig({apiKey: "471944d3711b25f9444e1bd872df39cf-us5",  server: "us5"});
 
 app.post("/", function(req, res){
     const firstName = req.body.name;
@@ -35,23 +34,35 @@ app.post("/", function(req, res){
       ]
     };
 
+    const apiKey = ""; //Enter your api key.
     //send mailchimp
     const jsonData = JSON.stringify(data);
     const url = "https://us5.api.mailchimp.com/3.0/lists/" + listId;
     const options = {
         method: "POST",
-        auth: "cnsynk:471944d3711b25f9444e1bd872df39cf-us5",
+        auth: "cnsynk:" + apiKey,
     }
 
     const request = https.request(url, options, function(response){
+        
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+        
         response.on("data", function(data){
             console.log(JSON.parse(data));
-        })
+        });
     });
 
     request.write(jsonData);
     request.end();
     
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
 });
 //Sending the signup.html file to the browser as soon as a request is made on localhost:3000
 app.get("/", function(req, res){
