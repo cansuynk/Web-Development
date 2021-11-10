c485501dffeaf39f57efad1fa378601438a5a69d
cansuynk <cansuyanik96@gmail.com>
2021-11-10 16:05:40 +0300

    cookies and sessions are added

--- Secrets/app.js
+++ Secrets/app.js
@@ -7,8 +7,14 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 //const encrypt = require('mongoose-encryption');
 //const md5 = require('md5');
+/*
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+*/
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const app = express();
 
@@ -19,19 +25,36 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: 'Our little secret.',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const databaseName = "userDB";
 const uri = "mongodb://localhost:27017/";
 mongoose.connect(uri + databaseName, {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
-    email: String,
+    username: String,
     password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 //add encrypt to the schema
 //userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
+
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -45,9 +68,21 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
 
 app.post("/register", function(req, res){
-
+    /*
     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
         
         const newUser = new User({
@@ -64,6 +99,7 @@ app.post("/register", function(req, res){
         });
     
     });
+    */
 
     /*
     const newUser = new User({
@@ -72,9 +108,21 @@ app.post("/register", function(req, res){
     });
     */
 
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log("Error in registering.",err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+
     
 });
-
+/*
+//bcrypt password
 app.post("/login", function(req, res){
     const username = req.body.username;
     //const password = md5(req.body.password);
@@ -94,6 +142,26 @@ app.post("/login", function(req, res){
         }
     });
 });
+*/
+
+app.post("/login", function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if (err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+
+});
+
 
 let port = 3000;
 app.listen(port, function(){
