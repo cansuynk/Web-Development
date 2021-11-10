411fc41d227e3aaa71930627505a700fb4e2edae
Cansu Yanık <cansuyanik@hotmail.com>
2021-11-11 00:00:17 +0300

    google authentication is added

--- Blog Website/app.js
+++ Blog Website/app.js
@@ -6,9 +6,8 @@ const ejs = require("ejs");
 const _ = require('lodash');
 const mongoose = require('mongoose');
 
-const port = 3000;
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome! In this website, you can write and publish a post. Just click \"Add New Content\" button!";
 const aboutContent = "Hello everyone! Welcome! My name is Cansu. I am computer engineer. I am currently improving myself in the full stack web development field. I like books, movies, drinking coffee, music, travelling and so on. Also I am a cat mother 😻. You can share your posts, writings or texts. Thank you for visiting :)";
 const contactContent = "You can contact with me via these addresses.";
 
@@ -100,6 +99,11 @@ app.get("/posts/:postId", function(req, res){
   */
 
 });
+
+let port = process.env.PORT;
+if (port === null || port === ""){
+    port = 3000;
+}
 
 app.listen(port, function() {
   console.log("Server started on port " + port);
